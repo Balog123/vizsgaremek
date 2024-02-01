fdc17976b8064e8bacfcb4439c1c6b993ee7b427
Balog123 <144344188+Balog123@users.noreply.github.com>
2024-02-01 16:57:28 +0100

    Delete routes directory

--- routes/oldalak.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-router.get("/regisztracio", (req, res) => {
-    res.sendFile("regisztracio.html", { root: "./public" })
-})
-
-module.exports = router
